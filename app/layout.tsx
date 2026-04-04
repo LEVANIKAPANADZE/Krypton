f6f1c727662cd168be31d0e9505448d48999cc6f
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-04-04 16:55:03 +0400

    Fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Header/>
-      <body className="min-h-full flex flex-col">{children}</body>
-      <Footer/>
+      
+      <body className="min-h-full">
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
+      
     </html>
   );
 }
