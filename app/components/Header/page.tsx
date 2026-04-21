db3cea677e982cdb9decfd4f76995bfb816d0296
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-04-21 18:21:06 +0400

    Added tablet design

--- app/components/Header/page.tsx
+++ app/components/Header/page.tsx
@@ -9,18 +9,26 @@ export default function Header() {
   ];
 
   return (
-    <header className="w-full flex justify-between items-center p-4">
-      <img src="/image.png" alt="logo" className="w-6 h-6 rounded-md" />
+    <header className="w-full flex justify-between items-center p-4 md:p-6">
+      <img
+        src="/image.png"
+        alt="logo"
+        className="w-6 h-6 rounded-md md:w-8 md:h-8"
+      />
 
-      <div className="flex gap-4">
+      <div className="flex gap-4 md:gap-6">
         {navItems.map((item, index) => (
           <Link href={item.path} key={index}>
-            <img src={item.icon} alt="nav" className="w-6 h-6" />
+            <img src={item.icon} alt="nav" className="w-6 h-6 md:w-7 md:h-7" />
           </Link>
         ))}
       </div>
 
-      <img src="/image.png" alt="logo" className="w-6 h-6 rounded-md" />
+      <img
+        src="/image.png"
+        alt="logo"
+        className="w-6 h-6 rounded-md md:w-8 md:h-8"
+      />
     </header>
   );
 }
