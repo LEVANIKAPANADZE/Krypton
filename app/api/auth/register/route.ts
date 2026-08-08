c3e7b0ad15f643ad823978669390695bba3180d0
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-08-08 23:40:17 +0400

    feat: add user registration API

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,26 +1,51 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
+import bcrypt from "bcrypt";
 
 export async function POST(request: Request) {
-  const { username, email, password } = await request.json();
+  try {
+    const { name, email, password } = await request.json();
 
-  if (!username || !email || !password) {
+    const cleanName = name?.trim();
+    const cleanEmail = email?.trim().toLowerCase();
+
+    if (!cleanName || !cleanEmail || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 },
+      );
+    }
+
+    const client = await clientPromise;
+    const db = client.db("data");
+    const users = db.collection("users");
+
+    const existingUser = await users.findOne({ email: cleanEmail });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 409 },
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await users.insertOne({
+      name: cleanName,
+      email: cleanEmail,
+      password: hashedPassword,
+      favorites: [],
+    });
+
     return NextResponse.json(
-      { message: "All fields are required" },
-      { status: 400 },
+      { message: "User created successfully" },
+      { status: 201 },
     );
-  }
-
-  const client = await clientPromise;
-  const db = client.db("data");
-  const users = db.collection("users");
-
-  const existingUser = await users.findOne({ email });
-
-  if (existingUser) {
+  } catch (error) {
     return NextResponse.json(
-      { message: "User already exists" },
-      { status: 409 },
+      { message: "Error appeared while registering..." },
+      { status: 500 },
     );
   }
 }
